146a3aa22e4e38e390f0386736f2394f43d90b95
Andrey Tychinskiy <andreytichinsky@gmail.com>
2021-12-04 20:29:19 +0300

    Добавляет коды http

--- src/constants.js
+++ src/constants.js
@@ -8,3 +8,12 @@ module.exports.ExitCode = {
   error: 1,
   success: 0,
 };
+
+module.exports.HttpCode = {
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401,
+};
+
