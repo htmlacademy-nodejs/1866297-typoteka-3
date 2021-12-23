950cc94e4c95141e40e521421bb2ca3efd54499a
Andrey Tychinskiy <andreytichinsky@gmail.com>
2021-12-23 15:33:48 +0300

    Добавляет функцию загрузки моков

---
+++ src/service/lib/get-mock-data.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const fs = require(`fs`).promises;
+const FILENAME = `mocks.json`;
+let data = [];
+
+const getMockData = async () => {
+  if (data.length > 0) {
+    return data;
+  }
+
+  try {
+    const fileContent = await fs.readFile(FILENAME);
+    data = JSON.parse(fileContent);
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+
+  return data;
+};
+
+module.exports = {
+  getMockData,
+};
