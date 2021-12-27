54bd35889eabb1f814b580b2646c641b97d04f73
Andrey Tychinskiy <andreytichinsky@gmail.com>
2021-12-27 13:35:30 +0300

    Добавляет базовое логирование

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,24 +1,51 @@
 "use strict";
 
 const express = require(`express`);
-const {API_PREFIX} = require(`../../constants.js`);
+const {API_PREFIX, HttpCode} = require(`../../constants.js`);
+const {getLogger} = require(`../lib/logger.js`);
 const routes = require(`../api`);
 const DEFAULT_PORT = 3000;
 
 const app = express();
+const logger = getLogger({name: `api`});
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  logger.debug(`Request on route ${req.url}`);
+  res.on(`finish`, () => {
+    logger.info(`Response status code ${res.statusCode}`);
+  });
+  next();
+});
+
 app.use(API_PREFIX, routes);
 
+app.use((req, res) => {
+  res.status(HttpCode.NOT_FOUND).send(`Not found`);
+  logger.error(`Route not found: ${req.url}`);
+});
+
+app.use((err, _req, _res, _next) => {
+  logger.error(`An error occurred on processing request: ${err.message}`);
+});
 
 module.exports = {
   name: `--server`,
   run(args) {
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
-    app.listen(port, () => {
-      console.log(`server running on port ${port}`);
-    });
+    try {
+      app.listen(port, (err) => {
+        if (err) {
+          return logger.error(`An error occurred on server creation: ${err.message}`);
+        }
+
+        return logger.info(`Listening to connections on ${port}`);
+      });
+    } catch (err) {
+      logger.error(`An error occurred: ${err.message}`);
+      process.exit(1);
+    }
   },
 };
