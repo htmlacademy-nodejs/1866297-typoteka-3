7cbae01dd0f7a3983d84b4b3b6a311e10ce123bf
Andrey Tychinskiy <andreytichinsky@gmail.com>
2022-03-15 09:44:07 +0300

    Исправляет зависимость команд от переменных окружения

--- src/service/lib/sequelize.js
+++ src/service/lib/sequelize.js
@@ -3,28 +3,31 @@
 const Sequelize = require(`sequelize`);
 const {DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT} = process.env;
 
-const somethingIsNotDefined = [
-  DB_NAME,
-  DB_USER,
-  DB_PASSWORD,
-  DB_HOST,
-  DB_PORT,
-].some((it) => it === undefined);
+module.exports = (() => {
 
-if (somethingIsNotDefined) {
-  throw new Error(`One or more environmental variables are not defined`);
-}
+  const somethingIsNotDefined = [
+    DB_NAME,
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    DB_PORT,
+  ].some((it) => it === undefined);
 
-module.exports = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
-  port: DB_PORT,
-  // указываем, с какой СУБД предстоит работать
-  dialect: `postgres`,
-  // настройки пула соединений
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 10000,
-    idle: 10000,
-  },
-});
+  if (somethingIsNotDefined) {
+    throw new Error(`One or more environmental variables are not defined`);
+  }
+
+  return new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    host: DB_HOST,
+    port: DB_PORT,
+    // указываем, с какой СУБД предстоит работать
+    dialect: `postgres`,
+    // настройки пула соединений
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 10000,
+      idle: 10000,
+    },
+  });
+})();
