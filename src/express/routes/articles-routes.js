f1ce2c24c1371c02bd3569887c41b13bcbc58a16
Andrey Tychinskiy <andreytichinsky@gmail.com>
2022-06-19 14:04:31 +0300

    Исправляет некорректный мерж мастера

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -32,7 +32,7 @@ articlesRouter.get(
     async (req, res) => {
       const {user} = req.session;
       const {id} = req.params;
-      const {page = 1} = req.query;
+      let {page = 1} = req.query;
       page = +page;
 
       const limit = USER_INTERFACE_SETTINGS.articlesPerPage;
